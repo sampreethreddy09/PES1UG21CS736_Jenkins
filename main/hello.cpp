747b6bf0086e11e6b6e0718e2984d19f8ffa8191
sampreethreddy09 <sampreethreddy09@gmail.com>
2024-03-09 18:02:25 +0530

    i've changed the hello.cpp - 1

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World I'm PES1UG21CS736\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
